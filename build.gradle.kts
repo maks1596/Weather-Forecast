be9016a39bcada5be7ebadd2694102ab4d724666
mshestoperov <Maxim.Shestoperov@dataart.com>
2022-04-13 12:17:28 +0300

    Add classic view to project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "7.1.3" apply false
-    id("com.android.library") version "7.1.3" apply false
-    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
-    id("org.jetbrains.kotlin.jvm") version kotlinVersion apply false
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register("clean", Delete::class) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,5 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
 rootProject.name = "Weather Forecast"
+
 include(":app")
 include(":model")
 include(":list:data:impl", ":list:model")
