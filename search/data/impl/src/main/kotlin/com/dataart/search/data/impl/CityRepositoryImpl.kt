916ba055b94f8edc1cdbe6243efd42fc1cb0778e
mshestoperov <Maxim.Shestoperov@dataart.com>
2022-05-31 17:39:34 +0300

    Added limit parameter to query

--- search/data/impl/src/main/kotlin/com/dataart/search/data/impl/CityRepositoryImpl.kt
+++ search/data/impl/src/main/kotlin/com/dataart/search/data/impl/CityRepositoryImpl.kt
@@ -10,6 +10,16 @@ class CityRepositoryImpl(
 ) : CityRepository {
 
     override suspend fun getCities(query: String) = withContext(ioDispatcher) {
-        geoService.directGeocoding(cityName = query)
+        geoService.directGeocoding(cityName = query, limit = CITIES_LIMIT)
+    }
+
+    private companion object {
+        /**
+         * Number of the locations in the API response
+         * (up to 5 results can be returned in the API response)
+         *
+         * [https://openweathermap.org/api/geocoding-api]
+         */
+        private const val CITIES_LIMIT = 5
     }
 }
